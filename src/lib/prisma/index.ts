a57c30c07f222c676f069885448ad00525249250
karanvrm <2023csb1127@iitrpr.ac.in>
2026-05-31 14:16:18 +0530

    Prisma change

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,19 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var cachedPrisma: PrismaClient;
+  var cachedPrisma: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient;
+const prisma =
+  global.cachedPrisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-} else {
-    if (!global.cachedPrisma) {
-        global.cachedPrisma = new PrismaClient();
-    }
+if (process.env.NODE_ENV !== "production") {
+  global.cachedPrisma = prisma;
+}
 
-    prisma = global.cachedPrisma;
-};
-
-export const db = prisma;
+export { prisma };
