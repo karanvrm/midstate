ea34a353ec3af222659bb854c3b0dee4ffadaf96
karanvrm <2023csb1127@iitrpr.ac.in>
2026-06-23 11:39:46 +0530

    analysis added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { Providers, WhatsAppWidget } from "@/components";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "@/components/ui/sonner";
+
 // @ts-ignore: side-effect import for global styles
 import "@/styles/globals.css";
+
 import { aeonik, cn, generateMetadata, inter } from "@/utils";
 
 export const metadata = generateMetadata();
@@ -22,23 +24,19 @@ export default function RootLayout({
                 )}
             >
                 <Providers>
-                    <Toaster richColors theme="dark" position="top-right" />
+                    <Toaster
+                        richColors
+                        theme="dark"
+                        position="top-right"
+                    />
+
                     {children}
+
                     <WhatsAppWidget />
+
+                    <Analytics />
                 </Providers>
             </body>
         </html>
     );
-};
-
-{/* <div className="absolute top-0 z-[-2] h-screen w-screen bg-background bg-[radial-gradient(100%_50%_at_50%_0%,rgba(168,85,247,0.13)_0,rgba(168,85,247,0)_50%,rgba(168,85,247,0)_100%)]"></div> */ }
-{/* <AnimatedBackground
-                    numSquares={6}
-                    maxOpacity={0.2}
-                    duration={10}
-                    repeatDelay={10}
-                    className={cn(
-                        "[mask-image:radial-gradient(800px_circle_at_center,black,transparent)]",
-                        "inset-0 w-full h-[100%] inset-y-[-30%] hidden md:block",
-                    )}
-                /> */}
+}
